d0cb61f91ae811b1c798ded070dd2e91b439308a
pletzjd <j.pletzer17@gmail.com>
2022-07-22 18:42:21 -0400

    removed unnecessary files from src, created basic header and nav bar

---
+++ portfolio/src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "./nav";
+
+function Header() {
+	return (
+		<div>
+			<h1> Jordan Pletzer</h1>
+			<Nav />
+		</div>
+	);
+}
+export default Header;
---
+++ portfolio/src/components/nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Nav() {
+	return (
+		<ul class="flex-row flex ">
+			<li>
+				<a href="#placeholder"> About Me</a>
+			</li>
+			<li>
+				<a href="#placeholder"> Portfolio</a>
+			</li>
+			<li>
+				<a href="#placeholder"> Contact</a>
+			</li>
+		</ul>
+	);
+}
+
+export default Nav;

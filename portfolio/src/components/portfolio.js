b23117b30a976786c198043e1a514ef3af6d44ce
pletzjd <j.pletzer17@gmail.com>
2022-07-23 20:41:21 -0400

    Added a link to the repo of the app. Changed some of the background imgaes to better reflect the app.

--- portfolio/src/components/portfolio.js
+++ portfolio/src/components/portfolio.js
@@ -8,41 +8,59 @@ function Portfolio() {
         </header>
 
         <section className="my-work">
-            <a className="card dateNight" href="https://richardzhang01.github.io/date-night-generator/">
-                <header class="card-header">
-                    <h5>Date Night Generator</h5>
+            <a href="https://richardzhang01.github.io/date-night-generator/" className="card dateNight">
+                <header className="card-header">
+                    <h5 >Date Night Generator</h5>
                 </header>
+                <div className="card-header">
+                    <a href="https://github.com/RichardZhang01/date-night-generator">Repo</a>
+                </div>
             </a>
 
-            <a className="card placeholder-1 card-small" href="https://github.com/pletzjd">
+            <a className="card chatbox card-small" href="https://coolchatbox.herokuapp.com">
                 <header className="card-header">
-                    <h5>Placeholder-1</h5>
+                    <h5>Cool Chatbox</h5>
                 </header>
+                <div className="card-header">
+                    <a href="https://github.com/zzzbia/Cool-Chatbox">Repo</a>
+                </div>
             </a>
 
-            <a className="card placeholder-2 card-small" href="https://github.com/pletzjd">
+            <a className="card quiz card-small" href="https://pletzjd.github.io/Code-quiz/">
                 <header className="card-header">
-                    <h5>Placeholder-2</h5>
+                    <h5>Coding Quiz</h5>
                 </header>
+                <div className="card-header">
+                    <a href="https://github.com/pletzjd/Code-quiz">Repo</a>
+                </div>
             </a>
 
-            <a className="card placeholder-3 card-small" href="https://github.com/pletzjd">
+            <a className="card schedule card-small" href="https://pletzjd.github.io/Work-Day-Scheduler/">
                 <header className="card-header">
-                    <h5>Placeholder-3</h5>
+                    <h5>Work Day Scheduler</h5>
                 </header>
+                <div className="card-header">
+                    <a href="https://github.com/pletzjd/Work-Day-Scheduler">Repo</a>
+                </div>
             </a>
 
-            <a className="card placeholder-4 card-small" href="https://github.com/pletzjd">
+            <a className="card placeholder-4 card-small" href="https://pletzjd.github.io/Weather-Dashboard/">
                 <header className="card-header">
-                    <h5>Placeholder-4</h5>
+                    <h5>Weather Dashboard</h5>
                 </header>
+                <div className="card-header">
+                    <a href="https://github.com/pletzjd/Weather-Dashboard">Repo</a>
+                </div>
             </a>
 
 			
-            <a className="card placeholder-4 card-small" href="https://github.com/pletzjd">
+            <a className="card notepad card-small" href="https://note-taker-pletzjd.herokuapp.com/">
                 <header className="card-header">
-                    <h5>Placeholder-5</h5>
+                    <h5>Note Taker</h5>
                 </header>
+                <div className="card-header">
+                    <a href="https://github.com/pletzjd/Note_Taker">Repo</a>
+                </div>
             </a>
         </section>
 		</div>

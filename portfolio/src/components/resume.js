b4034ba0e6c7a87a1e068f593c9b0af9a7a26a56
pletzjd <j.pletzer17@gmail.com>
2023-03-01 14:51:16 -0400

    renamed resume file

--- portfolio/src/components/resume.js
+++ portfolio/src/components/resume.js
@@ -1,5 +1,5 @@
 import React from "react";
-import resume from '../files/Resume - Jordan Pletzer.docx .pdf'
+import resume from '../files/Resume - Jordan Pletzer.pdf'
 
 function Resume() {
 	return (

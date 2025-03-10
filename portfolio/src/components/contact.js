6f412558aa1621d1d36b2c38284c859f9abed736
pletzjd <j.pletzer17@gmail.com>
2025-03-10 01:56:41 -0400

    Updating format styling for contact page

--- portfolio/src/components/contact.js
+++ portfolio/src/components/contact.js
@@ -9,99 +9,99 @@ function Contact() {
 	const [message, setMessage] = useState('');
 	const [errorMessage, setErrorMessage] = useState('');
 	const form = useRef();
-  
+
 	const handlefocusChange = (e) => {
-	  const { target } = e;
-	  const inputType = target.name;
-	  const inputValue = target.value;
-  
-	  if (inputType === 'email' && inputValue === '') {
-		setErrorMessage('Email field is required');
-	  } else if(inputType === 'email' && !validateEmail(email)){
-		setErrorMessage('Invalid email ');
-	  } else if (inputType === 'name' && inputValue === '') {
-		setErrorMessage('Name field is required');
-	  } else if (inputType === 'message' && inputValue === '') {
-		setErrorMessage('Message field is required');
-	  }else{
-		setErrorMessage('')
-	  }
+		const { target } = e;
+		const inputType = target.name;
+		const inputValue = target.value;
+
+		if (inputType === 'email' && inputValue === '') {
+			setErrorMessage('Email field is required');
+		} else if (inputType === 'email' && !validateEmail(email)) {
+			setErrorMessage('Invalid email ');
+		} else if (inputType === 'name' && inputValue === '') {
+			setErrorMessage('Name field is required');
+		} else if (inputType === 'message' && inputValue === '') {
+			setErrorMessage('Message field is required');
+		} else {
+			setErrorMessage('')
+		}
 	};
 
 	const handleInputChange = (e) => {
 		const { target } = e;
 		const inputType = target.name;
 		const inputValue = target.value;
-	
+
 		if (inputType === 'email') {
-		  setEmail(inputValue);
+			setEmail(inputValue);
 		} else if (inputType === 'name') {
-		  setName(inputValue);
+			setName(inputValue);
 		} else if (inputType === 'message') {
-		  setMessage(inputValue);
+			setMessage(inputValue);
 		}
-	  };
-  
+	};
+
 	const handleFormSubmit = (e) => {
 
-	  e.preventDefault();
-  
-	  if (!validateEmail(email) || !name || !message) {
-		setErrorMessage('Form incomplete');
+		e.preventDefault();
 
-		return;
-	  }
+		if (!validateEmail(email) || !name || !message) {
+			setErrorMessage('Form incomplete');
 
-	  emailjs.sendForm(process.env.REACT_APP_emailjsServiceID, process.env.REACT_APP_emailjsTemplateID, form.current, process.env.REACT_APP_emailjsKey)
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
+			return;
+		}
 
-	  alert(`Message Sent!`);
-  
-	  setName('');
-	  setMessage('');
-	  setEmail('');
+		emailjs.sendForm(process.env.REACT_APP_emailjsServiceID, process.env.REACT_APP_emailjsTemplateID, form.current, process.env.REACT_APP_emailjsKey)
+			.then((result) => {
+				console.log(result.text);
+			}, (error) => {
+				console.log(error.text);
+			});
+
+		alert(`Message Sent!`);
+
+		setName('');
+		setMessage('');
+		setEmail('');
 	};
 
 	return (
 		<div>
 			<h3>Contact Me</h3>
 			<form ref={form} className="form">
-        <input
-          value={name}
-          name="name"
-		  onChange={handleInputChange}
-          onBlur={handlefocusChange}
-          type="text"
-          placeholder="Name"
-        />
-		<input
-          value={email}
-          name="email"
-		  onChange={handleInputChange}
-          onBlur={handlefocusChange}
-          type="email"
-          placeholder="Email"
-        />
-        <textarea
-          value={message}
-          name="message"
-		  className="message"
-		  onChange={handleInputChange}
-          onBlur={handlefocusChange}
-          type="text"
-          placeholder="Message"
-        />
-        <button type="button" onClick={handleFormSubmit}>Submit</button>
-      </form>
-      {errorMessage && (
-        <div>
-          <p>{errorMessage}</p>
-        </div>
-      )}
+				<input
+					value={name}
+					name="name"
+					onChange={handleInputChange}
+					onBlur={handlefocusChange}
+					type="text"
+					placeholder="Name"
+				/>
+				<input
+					value={email}
+					name="email"
+					onChange={handleInputChange}
+					onBlur={handlefocusChange}
+					type="email"
+					placeholder="Email"
+				/>
+				<textarea
+					value={message}
+					name="message"
+					className="message"
+					onChange={handleInputChange}
+					onBlur={handlefocusChange}
+					type="text"
+					placeholder="Message"
+				/>
+				<button type="button" onClick={handleFormSubmit}>Submit</button>
+			</form>
+			{errorMessage && (
+				<div className="form-error">
+					<p>{errorMessage}</p>
+				</div>
+			)}
 		</div>
 	);
 }
